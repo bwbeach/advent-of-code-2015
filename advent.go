ddc7b6212f8483d319ef7204333b2c2a368dc3d8
Brian Beach <coder@beachfamily.net>
2022-02-26 08:39:48 -0500

    day 1 part 1

--- advent.go
+++ advent.go
@@ -3,6 +3,8 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"os"
 )
 
 // computeFloor calculates the final floor of the elevator.
@@ -24,5 +26,15 @@ func computeFloor(text string) (int, error) {
 }
 
 func main() {
-	fmt.Println("hello")
+	text, err := ioutil.ReadFile("day01_input.txt")
+	if err != nil {
+		fmt.Printf("Error reading input: %s\n", err.Error())
+		os.Exit(1)
+	}
+	floor, err := computeFloor(string(text))
+	if err != nil {
+		fmt.Printf("Error computing floor: %s\n", err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf("floor = %d\n", floor)
 }
